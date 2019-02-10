892a8d5d3c3bf13e30c8ff94f7bd9c0f65a12191
Jamie Ingram <jr-ingram@hotmail.co.uk>
2019-02-10 16:29:53 +0000

    Initial Commit - Added Menu

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void ButtonHandlerPlay(){
+		//Will load a new level!
+		//AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Level1");
+	}
+	
+	public void ButtonHandlerLevel(){
+		//Loads the Level Selection Menu
+		AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("LevelSelection");
+	}
+}
