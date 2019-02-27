77c7a0f4df0c4872c4bfd3acaa0dea2748bcd173
Jamie Ingram <jr-ingram@hotmail.co.uk>
2019-02-27 12:16:37 +0000

    Added robots being harmable

---
+++ Assets/Scripts/BasicRobotController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicRobotController : MonoBehaviour
+{
+    public void onHit() {
+        Debug.Log("YAAAAAAAAAOW");
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/PlayerInteractionController.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+
+public class PlayerInteractionController : MonoBehaviour {
+    public Sprite crosshairImage;
+    public Sprite crosshairSelectable;
+    public Image crosshair;
+    public GraphicRaycaster graphicRaycaster;
+
+    void Update()
+    {
+        PhysicsRaycasts();
+    }
+
+    void PhysicsRaycasts()
+    {
+        Vector3 centreOfScreen = new Vector3 (Screen.width * 0.5f, Screen.height * 0.5f, 0);
+        float distanceToFireRay = 20;
+        Ray centreOfScreenRay = Camera.main.ScreenPointToRay(centreOfScreen);
+        RaycastHit hit;
+        if (Physics.Raycast(centreOfScreenRay, out hit, distanceToFireRay))
+        {
+            //ToggleSelectedCursor(true);
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("Raycast hit: " + hit.transform.name);
+                GameObject hitObject = hit.transform.gameObject;
+                if (hitObject.tag == "Enemy") {
+                    hitObject.GetComponent<BasicRobotController>().onHit();
+                    Debug.Log(hitObject);
+                    Debug.Log("Bang bang!");
+                    //hitObject.onHit();
+                }
+
+            }  
+        }
+    }
+
+    void GraphicsRaycasts()    {
+        PointerEventData eventData = new PointerEventData(EventSystem.current);
+        eventData.position = new Vector2(Screen.width * 0.5f, Screen.height * 0.5f);
+        List<RaycastResult> results = new List<RaycastResult>();
+        graphicRaycaster.Raycast(eventData, results);
+        bool hitButton = false;
+        if(results.Count > 0)
+        {
+            for(int i = 0; i < results.Count; i++)
+            {
+                Button button = results[i].gameObject.GetComponent<Button>();
+                if(button != null)
+                {
+                    hitButton = true;
+                    if(Input.GetMouseButtonDown(0)) button.onClick.Invoke();
+                }
+            }
+            if(hitButton)
+            {
+               ToggleSelectedCursor(true);
+                /* change this to the function that you use to
+                                                            change the cursor */
+            }
+        }
+    }
+
+    void ToggleSelectedCursor(bool interactable)
+    {
+        if (interactable)
+        {
+            crosshair.sprite = crosshairSelectable;
+        }
+        else {
+            crosshair.sprite = crosshairImage;
+        }
+    }
+}
