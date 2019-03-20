628ae788d005c9a6cf35f2c02d5107b6d7efa60d
Jamie Ingram <jr-ingram@hotmail.co.uk>
2019-03-20 19:44:47 +0000

    Moving PlayerInteraction to Player object and addition of DeathCollider script

---
+++ Assets/Scripts/DeathColliderScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathColliderScript : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.GetComponent<PlayerInteractionController>().getHit(100);
+        }
+    }
+}
